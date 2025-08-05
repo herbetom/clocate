421526128ec83136d01e4862ce0185c3ff062948
Tom Herbers <mail@tomherbers.de>
2025-08-05 17:16:13 +0200

    fixup! WIP: action

---
+++ .github/combine-artifacts.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+function parse_package_string() {
+	local input="$1"
+
+	# Fail if input does not end with -packages
+	if [[ "$input" != *-packages ]]; then
+		echo "Error: input must end with '-packages'"
+		exit 1
+	fi
+
+	# Remove trailing -packages
+	base="${input%-packages}"
+
+	# Regex match version and architecture
+	if ! [[ "$base" =~ ^(openwrt-[0-9]+\.[0-9]+|v?[0-9]+\.[0-9]+\.[0-9]+|SNAPSHOT|main)-(.+)$ ]]; then
+		echo "Could not parse input: $input"
+		exit 1
+	fi
+
+	local version="${BASH_REMATCH[1]}"
+	local arch="${BASH_REMATCH[2]}"
+
+	[[ "$version" == "SNAPSHOT" ]] && version="snapshots"
+
+	echo "combined/${version}/packages/${arch}/"
+}
+
+
+# loop over artifacts
+for d in artifacts/*/ ; do
+	# remove trailing slash
+	s="${d%/}"
+	# remove dir prefix
+	s="${s#artifacts/}"
+	# get new location
+	p=$(parse_package_string "${s}")
+
+	mkdir -p "${p}"
+	mv -v "${d}"/* "${p}"
+done
